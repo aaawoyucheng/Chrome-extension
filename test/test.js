c794b2ea6800d00267bfbb462521e70ad639508f
aaawoyucheng <aaa0376@163.com>
2016-04-01 16:01:07 +0800

    a

--- test/test.js
+++ test/test.js
@@ -1,9 +1,1 @@
-function dl(ml, mi) {
-    mi = mi.replace('&lt;', '<');
-    mi = mi.replace('&#62;', '>');
-    mi = mi.replace('&#38;', '&');
-    var ot = "";
-    for (var j = 0; j < mi.length; j++) ot += String.fromCharCode(ml[mi.charCodeAt(j) - 48]);
-    location.href = ot;
-    // console.log(ot);
-}
+document.title=$('.bc-box h2').text()+$('.on .lessonmbers').text()+$('.on h2 a').text()
