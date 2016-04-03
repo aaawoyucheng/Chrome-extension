f3026e5dad8a202b22e087a5e43735f9bea3d014
aaawoyucheng <aaa0376@163.com>
2016-04-03 19:34:05 +0800

    a

--- bilibili/bilibili.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-C:\Python34\python %~dp0bilibili.py  %1
-pause
--- bilibili/bilibili.js
+++
@@ -1,4 +0,0 @@
-a=document.createElement('iframe');
-a.style.display='none';
-a.src="bilibili://"+document.URL;
-document.body.appendChild(a);
