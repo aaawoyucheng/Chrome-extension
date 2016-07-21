9f0cec46d1aa3792a2d703297651c0c82ce35f2a
hm <aaa0376@163.com>
2016-07-21 14:44:27 +0800

    add

---
+++ 插件/关灯看视频/background.js
@@ -0,0 +1,24 @@
+﻿function turn(tabId) {
+    chrome.tabs.executeScript(tabId, {
+        file: "turnoff.js"
+    });
+
+}
+chrome.pageAction.onClicked.addListener(function(tabs) {
+    turn(tabs.id);
+});
+
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+    if (tab.url.match(/bilibili|tudou/i)) {
+        if (tabId != null) {
+            chrome.pageAction.show(tabId);
+            chrome.tabs.executeScript(tabId, {
+                file: "jquery-1.9.1.min.js"
+            });
+            console.log(localStorage['auto']);
+            if (localStorage['auto'] == 'true') {
+                turn(tabId);
+            }
+        }
+    }
+});
