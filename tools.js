99618846dfa9f3e25e3245a49f2f03838d4df947
hm <aaa0376@163.com>
2016-08-04 22:22:18 +0800

    tools update

--- tools.js
+++ tools.js
@@ -1,3 +1,7 @@
 $.fn.clean=function(){
+    var max_width=$(this).width();
     $(this).siblings().hide();
+    $(this).parents().siblings().hide();
+    $(this).parents().css('cssText',function(){return 'max-width:'+max_width+'px!important'});
+    $(this).parents().css('cssText',function(){return 'background:white!important'});
 }
