9a1dd3a0de2c443128362e16a272d8535e2a3695
hm <aaa0376@163.com>
2016-08-27 22:39:42 +0800

    ad

--- test/test.js
+++ test/test.js
@@ -1,6 +1,19 @@
 function get(url,selector){
     var html=$.get(url);
-    var dom=$((new DOMParser()).parseFromString(html, 'text/html'));//.find(selector);
+    var dom=$((new DOMParser()).parseFromString(html, 'text/html'))//.find(selector);
+    console.log(dom);
     return dom;
 }
-console.log(get('http://acg.178.com/201106/102559759807.html','.news_box'));
+$('.next').remove();
+$('.cms_pages').each(function(i,o){
+    $('<div class=next link='+o.href+'></div>').insertBefore('.relevant_information');
+})
+$('.next').each(function(i,o){
+    var dom=$(o);
+    $.ajax({
+        url:dom.attr('link'),
+        success:function(msg){
+            dom.html($((new DOMParser()).parseFromString(msg, 'text/html')).find('.newstext'));
+        }
+    });
+});
