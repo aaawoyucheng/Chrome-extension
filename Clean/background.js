5421fedd030b73e04f83e42f2e80e19d39a354cc
hm <aaa0376@163.com>
2016-09-06 22:18:19 +0800

    aaa

--- Clean/background.js
+++ Clean/background.js
@@ -1,21 +1,28 @@
-﻿var run = function() {
-    chrome.tabs.insertCSS({
-        file: 'typo.css'
-    });
+﻿var injectCss = function(href) {
     chrome.tabs.executeScript({
-        file: "jquery-1.9.1.min.js"
+        code:
+            'var elm = document.createElement("link");' +
+            'elm.type = "text/css";' +
+            'elm.rel = "stylesheet";' +
+            'elm.href = "'+href+'";' +
+            'document["head"].appendChild(elm);'
     });
-    chrome.tabs.executeScript({
-        file: "clean.js"
-    });
+}
 
+var run = function() {
+    injectCss(chrome.extension.getURL('typo.css'));
+    chrome.tabs.executeScript({
+        file:'jquery-1.9.1.min.js'
+    });
+    chrome.tabs.executeScript({
+        file:'clean.js'
+    });
 }
 
 chrome.browserAction.onClicked.addListener(function(tab) {
     run();
 });
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-    console.log(localStorage['auto']);
     if (localStorage['auto'] == 'true') {
         run();
     }
