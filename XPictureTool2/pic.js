37705e82c2341d92bd999b33f7baf91f442a7495
hm <aaa0376@163.com>
2016-10-05 01:04:50 +0800

    a

--- XPictureTool2/pic.js
+++ XPictureTool2/pic.js
@@ -1,57 +1,69 @@
-//初始化$('#imgcontent')
-(function() {
+var changeShow=function(){
+    if (!$('#imgcontent').hasClass('display_none')) {
+        $('#imgcontent').addClass('display_none');
+        $('#imgcontent').siblings().removeClass('display_none');
+    } else {
+        $('#imgcontent').siblings().addClass('display_none');
+        $('#imgcontent').removeClass('display_none');
+    }
+}
+var init = function() {
     if ($('#imgcontent').size() == 0) {
+        var cssText =
+            "#imgcontent{background:#fcfcfe;text-align: center;padding:1px 24px;margin: auto;width: 720px;position: absolute;top: 0;right: 0;left: 0}" +
+            "#imgcontent_title{border:none;font-weight:bold;line-height:  30px;font-size:21px;padding:20px 0px;border-bottom: 1px solid rgba(233, 230, 232, 0.37)}" +
+            ".display_none{display:none!important}" +
+            ".display_img{width: 720px;margin: 20px auto;border-radius: 8px;background: black}" +
+            ".display_img img{max-width: 720px;display: block;margin:auto}";
+        $('<style class=xpictureCss >' + cssText + '</style>').appendTo('head');
         $('<div id="imgcontent" ></div>').appendTo('body');
+        var imgcontent = $('#imgcontent');
+        changeShow();
     }
-    $('<style class=xpictureCss >\
-    #imgcontent{background:#fcfcfe;text-align: center;padding:1px 24px;margin: auto;width: 720px;position: absolute;top: 0;right: 0;left: 0}\
-    #imgcontent_title{border:none;font-weight:bold;line-height:  30px;font-size:21px;padding:20px 0px;border-bottom: 1px solid rgba(233, 230, 232, 0.37)}\
-    .display_none{display:none!important}\
-    .display_img{width: 720px;margin: 20px auto;border-radius: 8px;background: black}\
-    .display_img img{max-width: 720px;display: block;margin:auto}\
-    </style>').appendTo('head');
     $('#imgcontent').html('<div id="imgcontent_title" >' + document.title + '</div>');
-})();
-var total = 1;
+    changeShow();
+}
+init();
+// var total = 1;
 
-function addImage(obj) {
-    console.log(obj);
-    if (obj.tagName == "IMG") {
-        var img = $("<img src=" + obj.src + " />");
-    } else if (typeof obj == 'string' & obj != '') {
-        var img = $('<img src=' + obj + ' />');
-    }
-    if (typeof img == "object") {
-        console.log();
-        if (img[0].naturalWidth >= 720) {
-            img.css('border-radius', '5px');
-        }
-        $("<div class='display_img'></div>")
-            .append(img)
-            .appendTo('#imgcontent');
-        // $("<div style=''>picture:" + total + "</div>")
-        //     .appendTo('#imgcontent');
-        total++;
-    }
-}
-if (/ /.exec(document.URL) != null) {
-    $('body img').each(function(i, o) {
-        if (o.naturalWidth > 200) addImage(o);
-    })
-}
-else if (/duitang.com/.exec(document.URL) != null) {
-    $('.j .mbpho .a img').each(function(i, o) {
-       addImage(o.src.replace(/thumb.+?\./,''));
-    })
-}
-else if (/diyidan.com/.exec(document.URL) != null) {
-    $('.post_content_img img').each(function(i, o) {
-       addImage(o.src.replace(/thumb.+?\./,''));
-    })
-}
- else {
-    $('body img').each(function(i, o) {
-        if (o.naturalWidth > 200) addImage(o);
-    })
-}
-$('#imgcontent').siblings().addClass('display_none');
+// function addImage(obj) {
+//     console.log(obj);
+//     if (obj.tagName == "IMG") {
+//         var img = $("<img src=" + obj.src + " />");
+//     } else if (typeof obj == 'string' & obj != '') {
+//         var img = $('<img src=' + obj + ' />');
+//     }
+//     if (typeof img == "object") {
+//         console.log();
+//         if (img[0].naturalWidth >= 720) {
+//             img.css('border-radius', '5px');
+//         }
+//         $("<div class='display_img'></div>")
+//             .append(img)
+//             .appendTo('#imgcontent');
+//         // $("<div style=''>picture:" + total + "</div>")
+//         //     .appendTo('#imgcontent');
+//         total++;
+//     }
+// }
+// if (/ /.exec(document.URL) != null) {
+//     $('body img').each(function(i, o) {
+//         if (o.naturalWidth > 200) addImage(o);
+//     })
+// }
+// else if (/duitang.com/.exec(document.URL) != null) {
+//     $('.j .mbpho .a img').each(function(i, o) {
+//        addImage(o.src.replace(/thumb.+?\./,''));
+//     })
+// }
+// else if (/diyidan.com/.exec(document.URL) != null) {
+//     $('.post_content_img img').each(function(i, o) {
+//        addImage(o.src.replace(/thumb.+?\./,''));
+//     })
+// }
+//  else {
+//     $('body img').each(function(i, o) {
+//         if (o.naturalWidth > 200) addImage(o);
+//     })
+// }
+// $('#imgcontent').siblings().addClass('display_none');
