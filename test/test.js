7d4dfe9b04ad327f16b529602833c530ff6c5c92
gm <aaa0376@163.com>
2017-04-01 17:27:45 +0800

    update

--- test/test.js
+++ test/test.js
@@ -1,13 +1,5 @@
-// $('.BDE_Image').each(function() {
-//     try {
-//         this.src = 'http://imgsrc.baidu.com/forum/' + this.src.match(/pic\/item.+/)[0];
-//         $(this).removeAttr('width').removeAttr('height').css('max-width', '700px');
-//     }catch(e) {}
-// });
-var total=$('.l_reply_num span:first').next().text();
-var i=0;
-while(i<total){
-    i++;
-    var url=window.location.hostname+window.location.pathname+'?pn='+i;
-    console.log(url);
-}
+var ff_info=JSON.parse(ff_urls);
+var title=ff_info['Vod'][0];
+var Data=ff_info['Data'];
+
+console.log(Data);
